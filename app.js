ead00dcc02342bb3fc4d5d358249498e9cf6c49b
Akshay <kumarakshay5042@gmail.com>
2023-02-22 17:43:11 +0530

    complete for now

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-var list_items = ["wake up"];
-const value = "Hello";
+var list_items = [];
+var work_list = [];
+const options_date = { weekday : 'long', month: 'short' ,day : 'numeric' }
+const day = new Date().toLocaleDateString('en-us' , options_date);
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -15,11 +18,23 @@ app.listen(5000 , function(){
 });
 
 app.get("/", function(req , res){
-    res.render('list' ,{key:value , list_items:list_items});
+    res.render('list' ,{ListTitle:day , list_items:list_items});
 });
 
 app.post("/", (req, res) =>{
-    list_items.push(req.body.list_item);
-    res.redirect("/");
+    if(req.body.list === "Work"){
+        work_list.push(req.body.list_item);
+        res.redirect("/work");
+
+    } else {
+        list_items.push(req.body.list_item);
+        res.redirect("/");
+    }
+
 })
+
+app.get("/work" , (req , res) =>{
+    res.render('list' ,{ListTitle : "Work" , list_items:work_list});
+})
+
 
