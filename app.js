24771f54e7fb977a5bdc106c21a3eee40f96186c
Akshay <kumarakshay5042@gmail.com>
2023-04-20 16:28:48 +0530

    Screen shots added

--- app.js
+++ app.js
@@ -2,9 +2,40 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-var list_items = [];
-var work_list = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemSchema = new mongoose.Schema({
+    name : String
+});
+
+let list_items = [];
+
+const Item = mongoose.model("item" , itemSchema);
+
+function insert_db(req){
+    
+    const item = new Item ({
+        name: req
+    });
+    item.save().then(() => {reload_list(list_items);});
+    
+}
+function reload_list(list){ 
+    Item.find((err , items) => {
+        if(err){
+            console.log(err);
+        } else {
+            list.length = 0;
+            items.forEach((item) => {
+
+                list.push(item.name);
+            });
+        }
+    });
+
+}
 const options_date = { weekday : 'long', month: 'short' ,day : 'numeric' }
 const day = new Date().toLocaleDateString('en-us' , options_date);
 
@@ -18,16 +49,24 @@ app.listen(5000 , function(){
 });
 
 app.get("/", function(req , res){
-    res.render('list' ,{ListTitle:day , list_items:list_items});
+    //here
+    reload_list(list_items)  ;    
+    setTimeout(() => {
+        res.render('list' ,{ListTitle:day , list_items:list_items});
+    }, 1000);
+    
 });
 
 app.post("/", (req, res) =>{
     if(req.body.list === "Work"){
+        // here
         work_list.push(req.body.list_item);
         res.redirect("/work");
 
     } else {
-        list_items.push(req.body.list_item);
+        //here
+        insert_db(req.body.list_item);
+        // list_items.push(req.body.list_item);
         res.redirect("/");
     }
 
